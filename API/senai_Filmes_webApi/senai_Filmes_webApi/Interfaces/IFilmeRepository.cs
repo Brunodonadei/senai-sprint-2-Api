b1bd1e7c91fd61ac8748f1c7950b305b37086a41
Bruno Donadei <domanbruno@gmail.com>
2021-04-06 07:50:27 -0300

    Adicionado a interface de GeneroRepository e FilmeRepository

---
+++ API/senai_Filmes_webApi/senai_Filmes_webApi/Interfaces/IFilmeRepository.cs
@@ -0,0 +1,54 @@
+﻿using senai_Filmes_webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_Filmes_webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório FilmeRepository
+    /// </summary>
+    interface IFilmeRepository
+    {
+        /// <summary>
+        /// Método que lista todos os filmes
+        /// </summary>
+        /// <returns></returns>
+        List<FilmeDomain> ListAll();
+
+        /// <summary>
+        /// Busca um filme pelo seu id
+        /// </summary>
+        /// <param name="id">id de um filme</param>
+        /// <returns></returns>
+        FilmeDomain SearchByid(int id);
+
+        /// <summary>
+        /// Cadastra um novo filme
+        /// </summary>
+        /// <param name="novoFilme">objeto chamado novoFilme com as informações que serão cadastradas</param>
+        void Register(FilmeDomain novoFilme);
+
+        /// <summary>
+        /// Atualiza um filme existente passando o id pelo corpo da requisição
+        /// </summary>
+        /// <param name="filme">Objeto filme com as novas informações</param>
+        void UpdateBodyId(FilmeDomain filme);
+
+        /// <summary>
+        /// Atualiza um filme existente passando o id pela url da requisição
+        /// </summary>
+        /// <param name="id">id do filme que será atualizado</param>
+        /// <param name="filme">objeto filme com as novas informações</param>
+        void UpdateUrlId(int id, FilmeDomain filme);
+
+        /// <summary>
+        /// Deleta um filme existente
+        /// </summary>
+        /// <param name="id">id do filme que será deletado</param>
+        void Delete(int id)
+        
+
+    }
+}
---
+++ API/senai_Filmes_webApi/senai_Filmes_webApi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,55 @@
+﻿using senai_Filmes_webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_Filmes_webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório GeneroRepository
+    /// </summary>
+    interface IGeneroRepository
+    {
+        // TipoRetorno NomeMetodo(TipoParametro NomeParametro);
+
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>Uma lista de gêneros</returns>
+        List<GeneroDomain> ListAll();
+
+        /// <summary>
+        /// Busca um gênero através do seu id
+        /// </summary>
+        /// <param name="id">id de um genero</param>
+        /// <returns>Um objeto genero que foi buscado</returns>
+        GeneroDomain SearchById(int id);
+
+        /// <summary>
+        /// Cadastra um novo gênero
+        /// </summary>
+        /// <param name="novoGenero">Objeto chamado novo genero com as informações que serão cadastradas</param>
+        void Register(GeneroDomain novoGenero);
+
+        /// <summary>
+        /// Atualiza um genero existente passando o id pelo corpo da requisição
+        /// </summary>
+        /// <param name="genero">Objeto genero com as novas informações</param>
+        void UpdateBodyId(GeneroDomain genero);
+
+        /// <summary>
+        /// Atualiza um genero existente passando o id pela url da requisição
+        /// </summary>
+        /// <param name="id">id do genero que será atualizado</param>
+        /// <param name="genero">objeto genero com as novas informações</param>
+        void UpdateUrlId(int id, GeneroDomain genero);
+
+
+        /// <summary>
+        /// Deleta um genero existente
+        /// </summary>
+        /// <param name="id">id do genero que será deletado</param>
+        void Delete(int id);
+    }
+}
